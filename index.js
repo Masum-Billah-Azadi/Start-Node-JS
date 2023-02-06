5d4cfebfd81b14b0515c3dc7fbc6f9f12fa52f15
Masum-Billah-Azadi <mdmasumbillahajadi1@gmail.com>
2023-02-06 19:35:22 +0600

    Start Node Program

---
+++ index.js
@@ -0,0 +1,5 @@
+const people = require('./test/people');
+console.log(people.people);
+console.log(people.a);
+people.test();
+
---
+++ test/people.js
@@ -0,0 +1,10 @@
+var people = ["Masum","Rahat","Tanjim"];
+var a = 10;
+function test (){
+    console.log("Succes Good job");
+}
+module.exports = {
+    people,
+    a,
+    test
+}
